ba0ff4405492d0003c524f130f23bb0e8c1bbc73
Joshua Mormann <josh@abrevd.com>
2018-11-28 20:30:22 -0500

    add state to Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,53 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from 'react';
+import { Link } from 'gatsby';
+import logo from '../img/logo.svg';
 
-const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-        <Link className="navbar-item" to="/contact">
-          Contact
-        </Link>
-        <Link className="navbar-item" to="/contact/examples">
-          Form Examples
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
-    </div>
-  </nav>
-)
+class Navbar extends React.Component {
+  
+  state = { showMenu: false }
+
+  toggleMenu = () => {
+    this.setState({
+      showMenu: !this.state.showMenu
+    })
+  }
+ 
+  render() {
+    const menuActive = this.state.showMenu ? 'is-active' : '';
+    const burgerActive = this.state.showMenu ? 'is-active' : '';
+    return (     
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <Link className="navbar-item" to="/">
+            <img src={logo} style={{ width: '88px' }} itemprop="image" alt="" />
+          </Link>
+          <div className={`navbar-burger burger ${burgerActive}`} onClick={this.toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        
+        <div className={`navbar-menu ${menuActive}`} >
+          <div className="navbar-start">
+            <Link className="navbar-item" to="/about">
+              About
+            </Link>
+            <Link className="navbar-item" to="/products">
+              Products
+            </Link>
+            <Link className="navbar-item" to="/contact">
+              Contact
+            </Link>
+            <Link className="navbar-item" to="/contact/examples">
+              Form Examples
+            </Link>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+};
 
 export default Navbar
+
